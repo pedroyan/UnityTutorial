65ea7b617b259e785987770512fb4a44631c3ec3
Pedro Yan Ornelas de Oliveira <pedroyane@gmail.com>
2017-09-27 09:36:08 -0300

    Concluido asteroide

--- Space Shooter/Assets/DestroyOnContact.cs
+++ Space Shooter/Assets/DestroyOnContact.cs
@@ -3,7 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyOnContact : MonoBehaviour {
+	public GameObject AsteroidExplosion;
+	public GameObject PlayerExplosion;
 	private void OnTriggerEnter(Collider other) {
+		if (other.tag == "Boundary") {
+			return;
+		}
+
+		if (other.tag == "Player") {
+			Instantiate(PlayerExplosion, other.transform.position, other.transform.rotation);
+		}
+
+		Instantiate(AsteroidExplosion, transform.position, transform.rotation);
 		DestroyObject(other.gameObject);
+		DestroyObject(gameObject);
 	}
 }
