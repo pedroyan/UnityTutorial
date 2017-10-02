cea4a1d613670295396feb476bf759994e4ed797
Pedro Yan Ornelas de Oliveira <pedroyane@gmail.com>
2017-10-02 09:09:16 -0300

    Game Controller

---
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	public GameObject Hazard;
+	public Vector3 SpawnValue;
+
+	public int HazardCount;
+	public float SpawnWait;
+	public float StartWait;
+	public float WaveWait;
+
+	private void Start() {
+		StartCoroutine(SpawnWaves());
+	}
+
+	IEnumerator SpawnWaves() {
+
+		yield return new WaitForSeconds(StartWait);
+
+		while (true) {
+			for (int i = 0; i < HazardCount; i++) {
+				var spawnPosition = new Vector3(Random.Range(-SpawnValue.x, SpawnValue.x), SpawnValue.y, SpawnValue.z);
+				Instantiate(Hazard, spawnPosition, Quaternion.identity);
+				yield return new WaitForSeconds(SpawnWait);
+			}
+			yield return new WaitForSeconds(WaveWait);
+		}
+	}
+}
